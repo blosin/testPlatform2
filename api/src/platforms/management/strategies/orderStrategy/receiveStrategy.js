7577a55355f1f77745e072098e11a758bbc51bea
ross <franco.velez@ross.com.ar>
2020-11-16 17:49:27 -0300

    se envia un mensaje a la cola de aws del propiio branch al cancelarr la novedad

--- api/src/platforms/management/strategies/orderStrategy/receiveStrategy.js
+++ api/src/platforms/management/strategies/orderStrategy/receiveStrategy.js
@@ -4,61 +4,63 @@ import { APP_BRANCH } from '../../../../utils/errors/codeError';
 import NewsTypeSingleton from '../../../../utils/newsType';
 
 class ReceiveStrategy extends NewsTypeStrategy {
-    constructor(newToSet) {
-        super(newToSet);
-        this.savedNew = { _id: newToSet.id };
-        this.typeId = NewsTypeSingleton.idByCod('receive_ord');
+  constructor(newToSet) {
+    super(newToSet);
+    this.savedNew = { _id: newToSet.id };
+    this.typeId = NewsTypeSingleton.idByCod('receive_ord');
+  }
+
+  findTrace() {
+    return this.savedNew.traces.find(
+      (trace) =>
+        trace.update.typeId == this.newToSet.typeId &&
+        trace.update.typeIdPrev == this.newToSet.typeIdPrev &&
+        trace.entity == this.entity
+    );
+  }
+
+  createObjectsUpdate(platformResult, isValid) {
+    try {
+      this.createTrace(platformResult, isValid);
+      this.trace.update.typeIdPrev = this.newToSet.typeIdPrev;
+      const findQuery = this.savedNew._id;
+      const updateQuery = {
+        typeId: this.typeId,
+        viewed: new Date(),
+        $push: { traces: this.trace }
+      };
+      const options = {};
+
+      if (!isValid) delete updateQuery.$set.typeId;
+
+      return { findQuery, updateQuery, options };
+    } catch (error) {
+      const msg = 'No se pudo generar el findQuery o updateQuery.';
+      const meta = { ...this.newToSet, error: error.toString() };
+      new CustomError(APP_BRANCH.SETNEWS, msg, this.uuid, meta);
     }
+  }
 
-    findTrace() {
-        return this.savedNew.traces.find((trace) =>
-            trace.update.typeId == this.newToSet.typeId &&
-            trace.update.typeIdPrev == this.newToSet.typeIdPrev &&
-            trace.entity == this.entity);
-    }
+  async manageNewType() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.savedNew = await this.findNew(this.newToSet.id);
+        const isValid = true;
+        const platformResult = await this.platform.receiveOrder(
+          this.savedNew.order
+        );
+        const { findQuery, updateQuery, options } = this.createObjectsUpdate(
+          platformResult,
+          isValid
+        );
 
-    createObjectsUpdate(platformResult, isValid) {
-        try {
-
-            this.createTrace(platformResult, isValid);
-            this.trace.update.typeIdPrev = this.newToSet.typeIdPrev;
-            const findQuery = { _id: this.savedNew._id };
-            const updateQuery = {
-                typeId: this.typeId,
-                viewed: new Date(),
-                $push: { traces: this.trace }
-            };
-            const options = {};
-
-            if (!isValid)
-                delete updateQuery.$set.typeId;
-
-            return { findQuery, updateQuery, options };
-        } catch (error) {
-            const msg = 'No se pudo generar el findQuery o updateQuery.';
-            const meta = { ...this.newToSet, error: error.toString() };
-            new CustomError(APP_BRANCH.SETNEWS, msg, this.uuid, meta);
-        }
-    }
-
-    async manageNewType() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                this.savedNew = await this.findNew(this.newToSet.id);
-                const isValid = true;
-                const platformResult = await this.platform
-                    .receiveOrder(
-                        this.savedNew.order);
-                const { findQuery, updateQuery, options } =
-                    this.createObjectsUpdate(platformResult, isValid);
-
-                await this.updateNew(findQuery, updateQuery, options);
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
+        await this.updateNew(findQuery, updateQuery, options);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
 
 export default ReceiveStrategy;
--- api/src/platforms/management/strategies/orderStrategy/retriveDriverStrategy.js
+++ api/src/platforms/management/strategies/orderStrategy/retriveDriverStrategy.js
@@ -1,71 +1,76 @@
-
 import NewsTypeStrategy from '../newsTypeStrategy';
 import NewsTypeSingleton from '../../../../utils/newsType';
 import CustomError from '../../../../utils/errors/customError';
 import { APP_BRANCH } from '../../../../utils/errors/codeError';
 
 class RetrieveDriverStrategy extends NewsTypeStrategy {
-    constructor(newToSet) {
-        super(newToSet);
-        this.savedNew = { _id: newToSet.id };
+  constructor(newToSet) {
+    super(newToSet);
+    this.savedNew = { _id: newToSet.id };
+  }
+
+  validateTransition() {
+    return this.savedNew.ownDelivery === false;
+  }
+
+  createObjectsUpdate(platformResult, isValid) {
+    try {
+      this.createTrace(platformResult, isValid);
+      const findQuery = this.savedNew._id;
+      const updateQuery = {
+        typeId: this.typeId,
+        viewed: null,
+        'order.driver': driver,
+        $push: { traces: this.trace }
+      };
+      const options = {};
+
+      if (!isValid) delete updateQuery.typeId;
+      return { findQuery, updateQuery, options };
+    } catch (error) {
+      const msg = 'No se pudo generar el findQuery o updateQuery.';
+      const meta = { ...this.newToSet, error: error.toString() };
+      new CustomError(APP_BRANCH.SETNEWS, msg, this.uuid, meta);
     }
+  }
 
-    validateTransition() {
-        return this.savedNew.ownDelivery === false;
-    }
+  async manageNewType() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.savedNew = await this.findNew(this.newToSet.id);
+        this.typeId = NewsTypeSingleton.idByCod('upd_ord');
 
-    createObjectsUpdate(platformResult, isValid) {
-        try {
-            this.createTrace(platformResult, isValid);
-            const findQuery = { _id: this.savedNew._id };
-            const updateQuery = {
-                typeId: this.typeId,
-                viewed: null,
-                'order.driver': driver,
-                $push: { traces: this.trace }
-            };
-            const options = {};
+        const isValid = this.validateTransition();
 
-            if (!isValid)
-                delete updateQuery.typeId;
-            return { findQuery, updateQuery, options };
-        } catch (error) {
-            const msg = 'No se pudo generar el findQuery o updateQuery.';
-            const meta = { ...this.newToSet, error: error.toString() };
-            new CustomError(APP_BRANCH.SETNEWS, msg, this.uuid, meta);
+        let platformResult = null;
+        if (isValid) {
+          platformResult = await this.platform.retriveDriver(
+            this.savedNew.order
+          );
         }
-    }
 
-    async  manageNewType() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                this.savedNew = await this.findNew(this.newToSet.id);
-                this.typeId = NewsTypeSingleton.idByCod('upd_ord');
+        const { findQuery, updateQuery, options } = this.createObjectsUpdate(
+          platformResult,
+          isValid
+        );
+        await this.updateNew(findQuery, updateQuery, options);
 
-                const isValid = this.validateTransition();
-
-                let platformResult = null;
-                if (isValid) {
-                    platformResult = await this.platform
-                        .retriveDriver(
-                            this.savedNew.order);
-                }
-
-                const { findQuery, updateQuery, options } =
-                    this.createObjectsUpdate(platformResult, isValid);
-                await this.updateNew(findQuery, updateQuery, options);
-
-                if (!isValid) {
-                    const msg = 'La novedad no cumple los requisitos de transición. ConsultaDelivery.';
-                    const meta = { ...this.newToSet, typeId: this.typeId, statusId: this.statusId };
-                    throw new CustomError(APP_BRANCH.SETNEWS, msg, this.uuid, meta);
-                }
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
+        if (!isValid) {
+          const msg =
+            'La novedad no cumple los requisitos de transición. ConsultaDelivery.';
+          const meta = {
+            ...this.newToSet,
+            typeId: this.typeId,
+            statusId: this.statusId
+          };
+          throw new CustomError(APP_BRANCH.SETNEWS, msg, this.uuid, meta);
+        }
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
 
 export default RetrieveDriverStrategy;
