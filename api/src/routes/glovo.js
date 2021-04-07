ace37808ffa3e52089847830a2d84ef6c2be8b0c
franco.velez.ross <franco.velez@ross.com.ar>
2021-04-07 13:47:35 -0300

    pipeline

--- api/src/routes/glovo.js
+++ api/src/routes/glovo.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 import controller from '../controllers/glovo';
 
-router.route('/orders')
-  .post(controller.saveCancelOrder);
+router.route('/orders').post(controller.saveCancelOrder);
 
-router.route('/orders/:id')
-  .get(controller.getOrder);
+router.route('/orders/:id').get(controller.getOrder);
 
 module.exports = router;
