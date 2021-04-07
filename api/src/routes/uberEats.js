17cca765648ec7096855dbd34065a41cf10e21f3
franco.velez.ross <franco.velez@ross.com.ar>
2021-04-07 14:11:03 -0300

    pipeline

--- api/src/routes/uberEats.js
+++ api/src/routes/uberEats.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 import controller from '../controllers/uberEats';
 
-router.route('/tracking')
-  .post(controller.getDeliveryInfo);
+router.route('/tracking').post(controller.getDeliveryInfo);
 
 module.exports = router;
