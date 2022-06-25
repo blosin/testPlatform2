af5f2d6c87888306e7c060a5acc85e7966f0d3e0
franco velez <francovf@smartfran.com>
2022-06-25 10:29:06 -0300

    deploy prod

--- api/src/platforms/sdk/pedidosYa/internals/webpack/prod.js
+++ api/src/platforms/sdk/pedidosYa/internals/webpack/prod.js
@@ -1,13 +1,4 @@
 module.exports = {
-    /*  target: 'node',
-     externals: ['aws-sdk'],
-     entry: slsw.lib.entries,
-     mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-     node: false,
-     optimization: {
-         minimize: false,
-     },
-     devtool: 'inline-cheap-module-source-map', */
-     mode: 'production'
+     mode: process.env.NODE_ENV == 'production' ?'production':'development'
  };
  
